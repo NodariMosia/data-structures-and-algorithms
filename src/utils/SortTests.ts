97870f2660cb80494925b0c04211f6d80e1f8e0b
Nodar Mosia <nodarmosia@gmail.com>
2023-11-01 00:49:09 +0400

    fix: randomize sorting test arrays

--- src/utils/SortTests.ts
+++ src/utils/SortTests.ts
@@ -1,11 +1,12 @@
+const ARRAY_SIZES = [0, 1, 2, 3, 5, 10, 100, 1000];
+
 export function testSortingFunction(sort: (array: number[]) => number[]) {
   it('should sort array', () => {
-    expect(sort([])).toEqual([]);
-    expect(sort([1])).toEqual([1]);
-    expect(sort([1, 1, 1, 1])).toEqual([1, 1, 1, 1]);
-    expect(sort([4, 3, 2, 1])).toEqual([1, 2, 3, 4]);
-    expect(sort([-1, 0, 2, 1])).toEqual([-1, 0, 1, 2]);
-    expect(sort([1, 5, -7, 2, 9, 0])).toEqual([-7, 0, 1, 2, 5, 9]);
-    expect(sort([1, 5, -7, 2, 9, 41, 0])).toEqual([-7, 0, 1, 2, 5, 9, 41]);
+    ARRAY_SIZES.forEach((length) => {
+      const array = Array.from({ length }, () => Math.floor(Math.random() * length));
+      const sortedArray = array.slice().sort((a, b) => a - b);
+
+      expect(sort(array)).toEqual(sortedArray);
+    });
   });
 }
