07a144f64835f10ae52baf313a25e7bbbfd8a93a
Nodar Mosia <nodarmosia@gmail.com>
2023-10-31 22:18:36 +0400

    test: improve ListTests

--- src/utils/ListTests.ts
+++ src/utils/ListTests.ts
@@ -17,6 +17,7 @@ export function testListImplementation<ListImpl extends List<number>>({
 
   test('constructor should create an empty list', () => {
     expect(list.length).toBe(0);
+    expect(list.toArray()).toEqual([]);
   });
 
   test('constructor should create a list with the specified items', () => {
@@ -29,42 +30,54 @@ export function testListImplementation<ListImpl extends List<number>>({
   test('insertFirst should add item to the beginning of the list', () => {
     list.insertFirst(10).insertFirst(20).insertFirst(30);
 
+    expect(list.length).toBe(3);
     expect(list.toArray()).toEqual([30, 20, 10]);
   });
 
   test('insertLast should add item to the end of the list', () => {
     list.insertLast(10).insertLast(20).insertLast(30);
 
+    expect(list.length).toBe(3);
     expect(list.toArray()).toEqual([10, 20, 30]);
   });
 
   test('insertAt should add item at the specified index', () => {
     list.insertLast(10).insertLast(20).insertLast(30).insertAt(40, 1);
 
+    expect(list.length).toBe(4);
     expect(list.toArray()).toEqual([10, 40, 20, 30]);
   });
 
-  test('removeFirst should remove the first item from the list', () => {
-    list.insertLast(10).insertLast(20).insertLast(30).removeFirst();
+  test('removeFirst should remove and return the first item from the list', () => {
+    list.insertLast(10).insertLast(20).insertLast(30);
 
+    expect(list.removeFirst()).toBe(10);
+    expect(list.length).toBe(2);
     expect(list.toArray()).toEqual([20, 30]);
   });
 
-  test('removeLast should remove the last item from the list', () => {
-    list.insertLast(10).insertLast(20).insertLast(30).removeLast();
+  test('removeLast should remove and return the last item from the list', () => {
+    list.insertLast(10).insertLast(20).insertLast(30);
 
+    expect(list.removeLast()).toBe(30);
+    expect(list.length).toBe(2);
     expect(list.toArray()).toEqual([10, 20]);
   });
 
-  test('removeAt should remove item at the specified index', () => {
-    list.insertLast(10).insertLast(20).insertLast(30).removeAt(1);
+  test('removeAt should remove and return the item at the specified index', () => {
+    list.insertLast(10).insertLast(20).insertLast(30);
 
+    expect(list.removeAt(1)).toBe(20);
+    expect(list.length).toBe(2);
     expect(list.toArray()).toEqual([10, 30]);
   });
 
   test('remove should remove the first occurrence of the specified item', () => {
-    list.insertLast(10).insertLast(20).insertLast(30).remove(20);
+    list.insertLast(10).insertLast(20).insertLast(30);
 
+    expect(list.remove(20)).toBe(true);
+    expect(list.remove(40)).toBe(false);
+    expect(list.length).toBe(2);
     expect(list.toArray()).toEqual([10, 30]);
   });
 
@@ -107,6 +120,7 @@ export function testListImplementation<ListImpl extends List<number>>({
   test('clear should remove all items from the list', () => {
     list.insertLast(10).insertLast(20).insertLast(30).clear();
 
+    expect(list.length).toBe(0);
     expect(list.toArray()).toEqual([]);
   });
 
