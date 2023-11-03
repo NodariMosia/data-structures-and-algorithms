4ec92847144e5ff26eb1370947c8c2e5f1aa1690
Nodar Mosia <nodarmosia@gmail.com>
2023-11-03 16:19:24 +0400

    test: include negative numbers in sort test arrays

--- src/utils/SortTests.ts
+++ src/utils/SortTests.ts
@@ -3,7 +3,13 @@ const ARRAY_SIZES = [0, 1, 2, 3, 5, 10, 100, 1000];
 export function testSortingFunction(sort: (array: number[]) => number[]) {
   it('should sort array', () => {
     ARRAY_SIZES.forEach((length) => {
-      const array = Array.from({ length }, () => Math.floor(Math.random() * length));
+      const array = Array.from({ length }, () => {
+        const sign = Math.random() < 0.5 ? -1 : 1;
+        const randomNum = Math.floor(Math.random() * 100_000);
+
+        return sign * randomNum;
+      });
+
       const sortedArray = array.slice().sort((a, b) => a - b);
 
       expect(sort(array)).toEqual(sortedArray);
